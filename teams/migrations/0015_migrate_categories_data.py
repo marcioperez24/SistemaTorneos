0108cccc338c5b1277953e518539c9547dbaca42
Marcio Perez <marcioperez24@gmail.com>
2026-09-22 20:25:06 -0600

    ActualizaciÃ³n automÃ¡tica

--- teams/migrations/0015_migrate_categories_data.py
+++ teams/migrations/0015_migrate_categories_data.py
@@ -18,17 +18,12 @@ def populate_and_migrate_categories(apps, schema_editor):
     # Create Categories
     category_instances = {}
     for code, name in CATEGORIAS_DICT.items():
-        cat = Categoria.objects.filter(nombre=name).first()
-        if not cat:
-            cat = Categoria.objects.create(nombre=name)
+        cat, created = Categoria.objects.get_or_create(nombre=name)
         category_instances[code] = cat
 
     # Fallback default category for anything else or senior
-    default_cat = category_instances.get('senior')
-    if not default_cat:
-        default_cat = Categoria.objects.filter(nombre='Senior / Libre').first()
-        if not default_cat:
-            default_cat = Categoria.objects.create(nombre='Senior / Libre')
+    default_cat = category_instances['senior']
+
 
 
     # Update Equipos
